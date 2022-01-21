bbe7ddd2d6c038c543913df662d06296f5d61603
abraham <agthankachan007@gmail.com>
2022-01-21 23:08:33 +0530

    3

--- mysite1/main/views.py
+++ mysite1/main/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 # Create your views here.
-def index(response):
+def test(response):
     return HttpResponse("<h1>Hello Friends</h1>")
 def v1(response):
     return HttpResponse("<h1>View 1!</h1>")
+def index(response,id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, "main/list.html", {"ls":ls})
+
+def home(response):
+    return render(response, "main/home.html", {})
