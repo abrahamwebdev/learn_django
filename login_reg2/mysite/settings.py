ec484c96d53df71e18cf9f78ee894c9bee1f7fd4
abraham <agthankachan007@gmail.com>
2022-02-03 11:57:48 +0530

    login register with crispy

--- login_reg2/mysite/settings.py
+++ login_reg2/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'main.apps.MainConfig',
     'register.apps.RegisterConfig',
 ]
@@ -125,3 +126,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK="bootstrap4"
