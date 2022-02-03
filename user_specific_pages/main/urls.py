7704863b922130c40af9e78122488003f49398b9
abraham <agthankachan007@gmail.com>
2022-02-03 23:53:44 +0530

    user speciic pages , access

---
+++ user_specific_pages/main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+path("<int:id>",views.index, name="index"),
+path("v1/",views.v1, name="view 1"),
+path("", views.home,name="home"),
+path("home/", views.home, name="home"),
+path("create/", views.create, name="create"),
+path("view/", views.view, name="view")
+
+
+]
