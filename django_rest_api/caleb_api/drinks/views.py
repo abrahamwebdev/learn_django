5d9ad2efa79172eb8bf02f6f16ffe05facadeb35
abraham <agthankachan007@gmail.com>
2022-09-29 19:47:17 +0530

    get and post

---
+++ django_rest_api/caleb_api/drinks/views.py
@@ -0,0 +1,24 @@
+from django.http import JsonResponse
+from .models import Drink
+from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+@api_view(['GET','POST'])
+def drink_list(request):
+  #get all Drinks
+  #Serialize them
+  #return json
+  if request.method=='GET':
+    drinks=Drink.objects.all()
+    serializer=DrinkSerializer(drinks,many=True)
+    #Data in list
+    # return JsonResponse(serializer.data,safe=False)
+    #Data in object
+    return JsonResponse({'drinks':serializer.data})
+  if request.method=='POST':
+    serializer=DrinkSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data,status=status.HTTP_201_CREATED)
